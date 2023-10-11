2789332ac0127edd5a8c631ccdc617a84e35ef41
Florian-Jungwirth <florian22jungwirth@gmail.com>
2023-10-11 15:56:55 +0200

    close #13

--- lowco-front/src/app/pages/settings/settings.page.ts
+++ lowco-front/src/app/pages/settings/settings.page.ts
@@ -14,8 +14,7 @@ export class SettingsPage implements OnInit {
   metric: number;
   first = true;
 
-  constructor(private router: Router, private authService: AuthService, private titleService: TitleService) {
-  }
+  constructor(private router: Router, private authService: AuthService, private titleService: TitleService) {}
 
   async ngOnInit() {
     this.isAdmin = await this.authService.isUserAdmin();
